2b3842a00efb0d90a74a88eae87517d605d072c3
Urhengulas <johann.hemmann@code.berlin>
2022-12-01 17:12:32 +0100

    2022 Day 01 Part 2

--- aoc22/src/bin/day01.rs
+++ aoc22/src/bin/day01.rs
@@ -1,10 +1,26 @@
 fn main() {
     const INPUT: &str = include_str!("day01.txt");
+
     let a = part_1(INPUT);
     dbg!(a);
+
+    let b = part_2(INPUT);
+    dbg!(b);
 }
 
 fn part_1(input: &str) -> u32 {
+    let c = parse(input);
+    c.into_iter().max().unwrap()
+}
+
+fn part_2(input: &str) -> u32 {
+    let mut a = parse(input);
+    a.sort_unstable();
+    let b = &a[a.len() - 3..a.len()];
+    b.into_iter().sum()
+}
+
+fn parse(input: &str) -> Vec<u32> {
     let a = input.lines().map(str::trim).collect::<Vec<_>>();
     let mut c = vec![0];
     let mut c_idx = 0;
@@ -18,7 +34,7 @@ fn part_1(input: &str) -> u32 {
             c[c_idx] += d;
         }
     }
-    c.into_iter().max().unwrap()
+    c
 }
 
 #[cfg(test)]
@@ -48,5 +64,30 @@ mod tests {
 
         // Assert
         assert_eq!(a, 24000);
+    }
+
+    #[test]
+    fn test_2() {
+        // Arrange
+        const INPUT: &str = "1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000";
+
+        // Act
+        let a = part_2(INPUT);
+
+        // Assert
+        assert_eq!(a, 45000);
     }
 }
