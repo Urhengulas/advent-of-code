bf7b080462ef4371eaa9c0ed2d3041bc7daa1340
Urhengulas <johann.hemmann@code.berlin>
2022-12-04 01:59:29 +0100

    2022 Day 03 Prepare

---
+++ aoc22/src/bin/day03.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let input = include_str!("day01.txt");
+    let part_1 = part_1(input);
+    let part_2 = part_2(input);
+    dbg!(part_1, part_2);
+}
+
+fn part_1(input: &str) -> () {
+    todo!()
+}
+
+fn part_2(input: &str) -> () {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        // Arrange
+        let input = "";
+
+        // Act
+        let a = part_1(input);
+
+        // Assert
+        assert_eq!(a, ());
+    }
+}
