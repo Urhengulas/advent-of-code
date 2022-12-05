44eb6a8a59645849ace42b3fdc9b76d73ebdde9e
Urhengulas <johann.hemmann@code.berlin>
2022-12-05 01:32:22 +0100

    2022 Day 04 Part 1

--- aoc22/src/bin/day04.rs
+++ aoc22/src/bin/day04.rs
@@ -1,41 +1,78 @@
+use std::ops::RangeInclusive;
+
 fn main() {
     let input = include_str!("day04.txt");
-    let part_1 = part_1(input);
-    let part_2 = part_2(input);
-    dbg!(part_1, part_2);
+    dbg!(part_1(input), part_2(input));
 }
 
-fn part_1(_input: &str) -> u32 {
-    todo!()
+fn part_1(input: &str) -> usize {
+    input
+        .lines()
+        .map(str::trim)
+        .map(parse)
+        .filter(fully_contains)
+        .count()
 }
 
 fn part_2(_input: &str) -> u32 {
     todo!()
 }
 
+fn parse(line: &str) -> [RangeInclusive<u32>; 2] {
+    let (a, b) = line.split_once(',').unwrap();
+
+    let (c, d) = a.split_once('-').unwrap();
+    let (e, f) = (c.parse().unwrap(), d.parse().unwrap());
+
+    let (g, h) = b.split_once('-').unwrap();
+    let (i, j) = (g.parse().unwrap(), h.parse().unwrap());
+
+    [e..=f, i..=j]
+}
+
+fn fully_contains([a, b]: &[RangeInclusive<u32>; 2]) -> bool {
+    let c = a.start();
+    let d = a.end();
+    let e = b.start();
+    let f = b.end();
+
+    // a contains b
+    let g = c <= e && d >= f;
+
+    // b contains a
+    let h = e <= c && f >= d;
+
+    g || h
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8";
+
     #[test]
     fn test_1() {
         // Arrange
-        let input = "";
 
         // Act
-        let a = part_1(input);
+        let a = part_1(INPUT);
 
         // Assert
-        assert_eq!(a, 0);
+        assert_eq!(a, 2);
     }
 
     #[test]
     fn test_2() {
         // Arrange
-        let input = "";
 
         // Act
-        let a = part_2(input);
+        let a = part_2(INPUT);
 
         // Assert
         assert_eq!(a, 0);
