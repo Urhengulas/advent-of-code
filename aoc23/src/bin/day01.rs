8154532bc722eaa8d2fdd5829bd70bb453513025
Johann Hemmann <johann.hemmann@code.berlin>
2023-12-01 06:10:05 +0100

    2023 Day 1 Part 1

--- aoc23/src/bin/day01.rs
+++ aoc23/src/bin/day01.rs
@@ -6,6 +6,25 @@ fn main() {
 }
 
 fn part_1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(str::trim)
+        .map(|line| {
+            line.chars()
+                .filter(|c: &char| c.is_numeric())
+                .collect::<Vec<_>>()
+        })
+        .map(|numbers| {
+            let a = numbers[0];
+            let b = numbers[numbers.len() - 1];
+            [a, b].into_iter().collect::<String>()
+        })
+        .map(|num_str| u32::from_str_radix(&num_str, 10).unwrap())
+        .sum()
+}
+
+    dbg!(a);
+
     todo!()
 }
 
