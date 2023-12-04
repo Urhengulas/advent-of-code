997f7856eff8755423b5b0f4b851cf2ae9af3179
Johann Hemmann <johann.hemmann@code.berlin>
2023-12-04 05:59:56 +0100

    Prepare 4

---
+++ aoc23/src/bin/day04.rs
@@ -0,0 +1,19 @@
+const INPUT: &str = include_str!("day04.txt");
+
+fn main() {
+    let input = "";
+
+    dbg!(part_1(input));
+    dbg!(part_2(input));
+
+    dbg!(part_1(INPUT));
+    dbg!(part_2(INPUT));
+}
+
+fn part_1(input: &str) -> u32 {
+    todo!()
+}
+
+fn part_2(input: &str) -> u32 {
+    todo!()
+}
