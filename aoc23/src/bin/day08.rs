6c883bae9c515d3200c2baf3fa07e4c3740d8cee
Johann Hemmann <johann.hemmann@code.berlin>
2023-12-08 06:18:10 +0100

    2023 Day 08 Part 1

--- aoc23/src/bin/day08.rs
+++ aoc23/src/bin/day08.rs
@@ -1,30 +1,73 @@
+use std::collections::HashMap;
+
 const INPUT: &str = include_str!("day08.txt");
 
 fn main() {
-    let input = "";
+    let input = "RL
+
+    AAA = (BBB, CCC)
+    BBB = (DDD, EEE)
+    CCC = (ZZZ, GGG)
+    DDD = (DDD, DDD)
+    EEE = (EEE, EEE)
+    GGG = (GGG, GGG)
+    ZZZ = (ZZZ, ZZZ)";
+
+    let input2 = "LLR
+
+    AAA = (BBB, BBB)
+    BBB = (AAA, ZZZ)
+    ZZZ = (ZZZ, ZZZ)";
 
     dbg!(part_1(input));
+    dbg!(part_1(input2));
     // dbg!(part_2(input));
 
-    // dbg!(part_1(INPUT));
+    dbg!(part_1(INPUT));
     // dbg!(part_2(INPUT));
 }
 
 fn part_1(input: &str) -> usize {
-    let a = input
+    let (a, b) = input.split_once("\n\n").unwrap();
+    // dbg!(a);
+    // dbg!(b);
+
+    let c = b
         .lines()
         .map(str::trim)
         .map(parse_line)
-        .collect::<Vec<_>>();
-    dbg!(&a);
+        .collect::<HashMap<_, _>>();
+    // dbg!(&c);
 
-    todo!()
+    let mut location = "AAA";
+    let mut step_count = 0;
+    for d in a.chars().cycle() {
+        if location == "ZZZ" {
+            break;
+        }
+
+        let e = match d {
+            'L' => 0,
+            'R' => 1,
+            _ => unreachable!(),
+        };
+        let f = c.get(location).unwrap();
+        let g = f[e];
+
+        location = g;
+        step_count += 1;
+    }
+
+    step_count
 }
 
 // fn part_2(input: &str) -> usize {
 //     todo!()
 // }
 
-fn parse_line(line: &str) -> u32 {
-    todo!()
+fn parse_line(line: &str) -> (&str, [&str; 2]) {
+    let (a, b) = line.split_once(" = ").unwrap();
+    let l = &b[1..4];
+    let r = &b[6..9];
+    (a, [l, r])
 }
