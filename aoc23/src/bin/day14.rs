1c148c9e139306a2f2c11eace3973de1fea82824
Johann Hemmann <johann.hemmann@code.berlin>
2023-12-14 07:00:55 +0100

    slide_north

--- aoc23/src/bin/day14.rs
+++ aoc23/src/bin/day14.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::fmt::{self, Debug};
 
 const INPUT: &str = include_str!("day14.txt");
 
@@ -13,8 +13,19 @@ O.#..O.#.#
 #....###..
 #OO..#....";
 
+const INPUT2: &str = "OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....";
+
 fn main() {
-    assert_eq!(dbg!(part_1(INPUT1)), 405);
+    assert_eq!(dbg!(part_1(INPUT1)), 136);
     // dbg!(part_1(INPUT));
 
     // assert_eq!(dbg!(part_2(INPUT1)), 400);
@@ -22,8 +33,18 @@ fn main() {
 }
 
 fn part_1(input: &str) -> usize {
-    let a = parse_input(input);
-    dbg!(&a);
+    let map = parse_input(input);
+    // dbg!(&map);
+
+    let north_map = slide_north(map);
+    // dbg!(&north_map);
+
+    for row in north_map {
+        for token in row {
+            print!("{token}")
+        }
+        println!()
+    }
 
     todo!()
 }
@@ -40,7 +61,7 @@ fn parse_line(line: &str) -> Vec<Token> {
     line.chars().map(Into::into).collect()
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 enum Token {
     Round,
     Cube,
@@ -56,4 +77,71 @@ impl From<char> for Token {
             _ => unreachable!(),
         }
     }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Empty => '.',
+                Self::Cube => '#',
+                Self::Round => 'O',
+            }
+        )
+    }
+}
+
+fn slide_north(mut map: Vec<Vec<Token>>) -> Vec<Vec<Token>> {
+    let num_rows = map.len();
+    let num_cols = map[0].len();
+    // dbg!(num_rows, num_cols);
+
+    for col_idx in 0..num_cols {
+        let mut col = map
+            .iter_mut()
+            .map(|line| &mut line[col_idx])
+            .collect::<Vec<_>>();
+        // dbg!(&col);
+
+        // 1. find cube and next cube
+        // 2. count round in-between cubes
+        // 3. write round below upper cube and empty until next cube
+        // 4. set cube to next cube and search next next cube
+        // 5. goto 2
+
+        let mut a = 0;
+        loop {
+            let b = match col[a..].iter().position(|token| **token == Token::Cube) {
+                Some(idx) => a + idx,
+                None => num_rows,
+            };
+            // dbg!(b);
+            // dbg!(a);
+
+            let c = &mut col[a..b];
+            // dbg!(&c);
+
+            let d = c.iter().filter(|token| ***token == Token::Round).count();
+            // dbg!(d);
+
+            for (e, f) in c.iter_mut().enumerate() {
+                **f = if e < d { Token::Round } else { Token::Empty }
+            }
+            // dbg!(&c);
+
+            if b == num_rows {
+                break;
+            } else if a == b {
+                a += 1;
+                continue;
+            } else {
+                a = b;
+            }
+        }
+        // dbg!(col);
+    }
+
+    map
 }
